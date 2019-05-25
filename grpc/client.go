3b37fdc4ffaac7d1f2ab3c3927513e56fc1b2654
ofk <farukarakaya@gmail.com>
2019-05-26 02:29:06 +0300

    client AddItem

--- grpc/client.go
+++ grpc/client.go
@@ -3,16 +3,12 @@ package grpc
 import (
 	"context"
 	pb "drcache/grpc/definitions"
-	"google.golang.org/grpc"
-	"log"
 )
 
-func AddItem(address string) pb.Reply {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
+type Client struct {
+	Clients map[string]pb.DrcacheClient
+}
 
+func (c *Client) AddItem(address string, request pb.AddRequest) (*pb.Reply, error) {
+	return c.Clients[address].Add(context.Background(), &request)
 }
